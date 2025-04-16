65660573017d322070b740725a712a9abc1e450d
Aarti-Mern-Stack <aarti.verma@designersx.com>
2025-04-16 06:39:51 +0530

    Update Store.js

--- src/lib/Store.js
+++ src/lib/Store.js
@@ -199,7 +199,7 @@ export const addDailyForecast = async (token, date, prediction, user_id) => {
   // console.log("adtaaa",token, date, prediction, user_id)
   try {
     const response = await axios.post(
-      `https://dev.astar8.com/p1g6s9ik3n/dly12g6ui8`,
+      `${Url}/p1g6s9ik3n/dly12g6ui8`,
       { user_id, date, prediction },
       {
         headers: {
@@ -403,7 +403,7 @@ export const publishButtonScheduled = async (e) => {
   };
   try {
     const response = await axios.post(
-      `https://dev.astar8.com/p1g6s9ik3n/45dsfr684t`,
+      `${Url}/p1g6s9ik3n/45dsfr684t`,
       finalData
     );
     const data = await response.data;
